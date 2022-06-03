74cc60e6fa488dab6d136f157094f4680f0d4cd4
CrispusKitala <chriskitala254@gmail.com>
2022-06-03 16:04:53 -0700

    a program that prints all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,6 +11,7 @@ int main(void)
 
 	for (number = 0; number <= 9; number++)
 	{
+		
 		putchar(number + '0');
 		if (number < 9)
 	{
@@ -18,6 +19,7 @@ int main(void)
 	putchar(' ');
 	}
 	}
+
 	putchar('\n');
 
 	return (0);
