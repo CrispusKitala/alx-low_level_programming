9b5bd9105ba6b7688f28cb1945cb04335996b8f6
CrispusKitala <chriskitala254@gmail.com>
2022-06-29 13:57:13 -0700

    a function that splits a string into words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,10 @@
 #include "main.h"
 #include <stdlib.h>
 
+int word_len(char *str);
+int count_words(char *str);
+char **strtow(char *str);
+
 /**
  * word_len - Locates the end of 1st word in a string
  * @str: Input string
